673c787e1fffe03a68f00ad60d1be82d8573a14b
beirui <beiruioh@gmail.com>
2023-04-05 13:45:28 +0800

    17

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ csrf.init_app(app)
 app.secret_key = "ming"
 #app.config['SECRET_KEY'] = "ming"
 app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Pa$$w0rd'
+app.config['MYSQL_USER'] = 'root' #change to admin
+app.config['MYSQL_PASSWORD'] = 'Pa$$w0rd' #change to 1234567890
 app.config['MYSQL_DB'] = 'pythonlogin'
 mysql = MySQL(app)
 

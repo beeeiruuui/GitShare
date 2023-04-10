3da10266e0150c78a5fd82e1a8759517caf8db8f
beeeiruuui <beiruioh@gmail.com>
2023-04-10 17:17:05 +0800

    111111111111aaaaaaaa

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ csrf.init_app(app)
 #database = Database()
 app.secret_key = "ming"
 #app.config['SECRET_KEY'] = "ming"
-app.config['MYSQL_HOST'] = 'test.cr1vhehorrch.ap-southeast-2.rds.amazonaws.com'
+app.config['MYSQL_HOST'] = 'awsdatabase.cr1vhehorrch.ap-southeast-2.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'admin' #change to root
 app.config['MYSQL_PASSWORD'] = 'Pa$$w0rd' #change to Pa$$w0rd
 app.config['MYSQL_DB'] = 'pythonlogin'
